0995f91e9d7d035036e8c222b1865835366ae2dc
crashdaddy <kennethscoggins@mail.com>
2020-02-26 14:16:44 -0600

    added jsdom-global to prevent window not defined error during tests

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Fetch-Unit-Testing
+
+Practice Testing Fetch
+
+For your practice of this section you're going:
+
+    to create a new repo called: " Fetch-Unit-Testing "
+    . clone the repo into your jsDev folder/directory
+    npm init Enter, enter, enter, enter, enter....
+    npm install -g mocha
+    Build a simple GUI app that uses this api: https://punkapi.com/documentation/v2.
+    use the video above to walk through building two tests for this simple app; 1 for a single beer and 1 for 10 beers.
+    You should end up with a simple GUI app that has at least two unit test injected into your fetch request.
---
+++ index.js
@@ -0,0 +1,89 @@
+const assert = require('assert');
+require('jsdom-global')()
+
+let beers = [];
+
+window.onload = () => {
+    getBeers(window.fetch);
+    
+}
+
+const getBeer = (fetch, id) => {
+    fetch('https://api.punkapi.com/v2/beers/' + id)
+    .then(res => {
+        if(!res.ok) {
+          throw Error(res.statusText)
+        } return res.json()
+      })
+    .then(data => { beers.push(data);
+        console.log(beers);
+        displayBeer();
+    })
+    .catch(err => document.getElementById("text").innerHTML= `Error,  ${err}`);
+
+}
+
+
+const getBeers = (fetch) => {
+    fetch('https://api.punkapi.com/v2/beers/')
+        .then(res => {
+            if(!res.ok) {
+              throw Error(res.statusText)
+            } return res.json()
+          })
+        .then(data => { beers.push(data);
+            console.log(beers);
+            displayBeer();
+        })
+        .catch(err => document.getElementById("text").innerHTML= `Error,  ${err}`);
+
+}
+
+const displayBeer = () => {
+    let imgText = "";
+    let textText = "";
+    for (let i = 0;i<beers[0].length;i++){
+        let beerImg = beers[0][i].image_url;
+        console.log(beerImg);
+        imgText += `<div style="width:50%;display:inline-block;" ><div style="float:left;display:inline-block;"><img src="${beerImg}" style="width:50px;"></div>
+                            <div>${beers[0][i].name}<br/>
+                          ${beers[0][i].description}<br/>
+                          ${beers[0][i].tagline}</div></div>`;
+    }
+    document.getElementById("img").innerHTML = imgText;
+}
+
+describe('getBeers', () => {
+    it('fetches API data', () => {
+        const fakeFetch = url => {
+            assert(url==='https://api.punkapi.com/v2/beers/')
+            return new Promise(function(resolve) {
+
+        })
+        }
+        
+       getBeers(fakeFetch);
+    })}),
+
+describe('getBeer', () => {
+    it('fetches the correct beer', (done) => {
+        const fakeFetch = url => {
+            assert(url==='https://api.punkapi.com/v2/beers/10')
+            
+            return Promise.resolve({
+                json: () => Promise.resolve({
+                results: [
+                    {"name":"Bramling X"}
+                ] 
+            })
+        })
+    }
+    getBeer(fakeFetch) 
+     .then(result => {
+        assert(result[0].name === 'Bramling X')
+        done();
+     })
+    })
+   })
+
+
